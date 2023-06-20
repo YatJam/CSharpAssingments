2cbf19c9f7915579dc55571c658a70974a4a98bd
james yates <yates_7@hotmail.com>
2023-06-20 16:31:17 +0100

    first assignment attempt calculator

---
+++ simpleCalculator/simpleCalculator/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("Well hello there...");
+Console.WriteLine("Please input the first number");
+int firstNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Please input the second number");
+int secondNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Select which operation to perform:");
+Console.WriteLine("[A]dd the two numbers together");
+Console.WriteLine("[S]ubtract the two numbers against eachother");
+Console.WriteLine("[M]ultiply the two numbers together");
+string operationInput = Console.ReadLine();
+
+string calculatorOutput(string opertationInput)
+{
+    if (operationInput == "A")
+    {
+        int result = firstNumber + secondNumber;
+        return (firstNumber + " + " + secondNumber + " = " + result);
+    }
+    else if (operationInput == "S")
+    {
+        int result = firstNumber - secondNumber;
+        return (firstNumber + " - " + secondNumber + " = " + result);
+        }
+    else if (operationInput == "M")
+    {
+        int result = firstNumber * secondNumber;
+        return (firstNumber + " * " + secondNumber + " = " + result);
+        }
+    else
+        return ("Invalid option selected");
+}
+
+Console.WriteLine(calculatorOutput(operationInput));
+
+Console.WriteLine("Press any key to end programme");
+Console.ReadLine();
